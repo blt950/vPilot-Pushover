01ef58e1d606d0384fa8a7a20a9808685aa6f731
Daniel <blt950@users.noreply.github.com>
2023-07-26 17:26:16 +0200

    Working very first alpha version

---
+++ Acars.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace vPilot_Pushover
+{
+    internal class Acars
+    {
+
+        public static List<Dictionary<string, string>> HoppieCache = new List<Dictionary<string, string>>();
+        public static Boolean CacheLoaded = false;
+        public static Main _main;
+
+        public string _callsign;
+        public string _logon;
+
+        public void init(Main main, String logon)
+        {
+            _main = main;
+            _logon = logon;
+
+            Timer hoppieTimer = new Timer();
+            hoppieTimer.Elapsed += new ElapsedEventHandler(FetchHoppie);
+            hoppieTimer.Interval = 45 * 1000;
+            hoppieTimer.Enabled = true;
+
+            // Run it manually once
+            FetchHoppie(null, null);
+        }
+
+        public void SetCallsign(String callsign)
+        {
+            _callsign = callsign;
+        }
+
+        async void FetchHoppie(object source, ElapsedEventArgs e)
+        {
+            string baseUrl = "http://www.hoppie.nl/acars/system/connect.html";
+            string logon = _logon;
+            string from = _callsign;
+            string type = "peek";
+            string to = "SERVER";
+
+            if(_callsign != null)
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Build the complete URL with GET variables
+                    string fullUrl = $"{baseUrl}?logon={logon}&from={from}&type={type}&to={to}";
+                    _main.SendDebug("[ACARS] Fetching Hoppie data");
+
+                    try
+                    {
+                        HttpResponseMessage response = await httpClient.GetAsync(fullUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseContent = await response.Content.ReadAsStringAsync();
+                            ParseHoppie(responseContent);
+                        }
+                        else
+                        {
+                            _main.SendDebug($"[ACARS] HttpResponse request failed with status code: {response.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _main.SendDebug($"[ACARS] An HttpResponse error occurred: {ex.Message}");
+                    }
+                }
+            } 
+            else
+            {
+                _main.SendDebug($"[ACARS] FetchHoppie aborted due to missing callsign");
+            }
+
+        }
+
+        void ParseHoppie(String response)
+        {
+
+            Boolean statusOk = response.StartsWith("ok");
+
+            if (statusOk)
+            {
+                foreach (Match match in Regex.Matches(response, @"\{(\d+)\s(\w+)\s(\w+)\s\{([^\}]+)\}\}"))
+                {
+                    // Initate the Regex groups
+                    string key = match.Groups[1].Value;
+                    string from = match.Groups[2].Value;
+                    string type = match.Groups[3].Value;
+                    string message = match.Groups[4].Value;
+
+                    // Clean the messages
+                    message = Regex.Replace(match.Groups[4].Value, @"\/data\d\/\d+\/\/.+\/", "");
+                    message = Regex.Replace(message, @"@", "");
+
+                    // Check if key doesnt' exist, then add it
+                    if (!HoppieCache.Exists(x => x["key"] == key))
+                    {
+                        // Create a dictionary for the current block and add the key-value pairs
+                        Dictionary<string, string> dataDict = new Dictionary<string, string>
+                        {
+                            { "key", key },
+                            { "from", from },
+                            { "type", type },
+                            { "message", message}
+                        };
+
+                        // Add the dictionary to the list
+                        HoppieCache.Add(dataDict);
+
+                        // Send the message to Pushover
+                        if (CacheLoaded == true && message != "")
+                        {
+                            _main.SendPushover($"[{type.ToUpper()}] {from}: {message}");
+                        }
+                    }
+
+
+                }
+
+                CacheLoaded = true;
+
+            }
+            else
+            {
+                _main.SendDebug("[ACARS] okCheck Error: " + response);
+            }
+
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.IO;
+
+using RossCarlson.Vatsim.Vpilot.Plugins;
+using RossCarlson.Vatsim.Vpilot.Plugins.Events;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Win32;
+
+namespace vPilot_Pushover
+{
+    public class Main : IPlugin
+    {
+        private IBroker _broker;
+        private static readonly HttpClient client = new HttpClient();
+        public string Name => "vPilot Pushover";
+        public string connectedCallsign = null;
+        private Acars acars;
+
+        // Settings
+        private Boolean SettingsLoaded = false;
+        private IniFile SettingsFile;
+
+        private String settingPrivateEnabled = null;
+        private String settingRadioEnabled = null;
+        private String settingHoppieEnabled = null;
+        private String settingHoppieLogon = null;
+        private String settingPushoverToken = null;
+        private String settingPushoverUser = null;
+
+        public void Initialize(IBroker broker)
+        {
+            _broker = broker;
+            LoadSettings();
+
+            SendDebug($"PushoverToken: {SettingsFile.Read("ApiKey", "Pushover")}");
+            SendDebug($"PushoverUser: {SettingsFile.Read("UserKey", "Pushover")}");
+
+            SendDebug($"HoppieEnabled: {SettingsFile.Read("Enabled", "Hoppie")}");
+            SendDebug($"HoppieLogon: {SettingsFile.Read("LogonCode", "Hoppie")}");
+
+            SendDebug($"PrivateEnabled: {SettingsFile.Read("Enabled", "RelayPrivate")}");
+            SendDebug($"RadioEnabled: {SettingsFile.Read("Enabled", "RelayRadio")}");
+
+            if (SettingsLoaded)
+            {
+                // Subscribe to events according to settings
+                _broker.NetworkConnected += OnNetworkConnectedHandler;
+                if (settingPrivateEnabled == "1") _broker.PrivateMessageReceived += OnPrivateMessageReceivedHandler;
+                if (settingRadioEnabled == "1") _broker.RadioMessageReceived += OnRadioMessageReceivedHandler;
+
+                // Enable ACARS if Hoppie is enabled
+                if (settingHoppieEnabled == "1")
+                {
+                    acars = new Acars();
+                    acars.init(this, settingHoppieLogon);
+                }
+
+                SendPushover("Connected");
+                SendDebug("Connected");
+
+            } else
+            {
+                SendDebug("vPilot Pushover plugin failed to load. Check your vPilot-Pushover.ini");
+            }
+
+        }
+
+        public void SendDebug(String text)
+        {
+            _broker.PostDebugMessage(text);
+        }
+
+        public async void SendPushover(String text)
+        {
+
+            var values = new Dictionary<string, string>
+            {
+                { "token", settingPushoverToken },
+                { "user", settingPushoverUser },
+                { "message", text }
+            };
+
+            var response = await client.PostAsync("https://api.pushover.net/1/messages.json", new FormUrlEncodedContent(values));
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            _broker.PostDebugMessage(responseString);
+
+        }
+
+        private void OnNetworkConnectedHandler(object sender, NetworkConnectedEventArgs e)
+        {
+            connectedCallsign = e.Callsign;
+            acars.SetCallsign(e.Callsign);
+            _broker.PostDebugMessage($"VATSIM connected with callsign: {e.Callsign}");
+        }
+
+        private void OnPrivateMessageReceivedHandler(object sender, PrivateMessageReceivedEventArgs e)
+        {
+            string from = e.From;
+            string message = e.Message;
+
+            SendPushover($"{from}: {message}");
+        }
+
+        private void OnRadioMessageReceivedHandler(object sender, RadioMessageReceivedEventArgs e)
+        {
+            string from = e.From;
+            string message = e.Message;
+
+            if (message.Contains(connectedCallsign))
+            {
+                SendPushover($"{from}: {message}");
+            }
+
+        }
+
+        private void LoadSettings()
+        {
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\vPilot");
+            if (registryKey != null)
+            {
+                string vPilotPath = (string)registryKey.GetValue("Install_Dir");
+                string configFile = vPilotPath + "\\Plugins\\vPilot-Pushover.ini";
+                SettingsFile = new IniFile(configFile);
+
+                // Set all values
+                settingPushoverToken = SettingsFile.Read("ApiKey", "Pushover");
+                settingPushoverUser = SettingsFile.Read("UserKey", "Pushover");
+                settingHoppieEnabled = SettingsFile.Read("Enabled", "Hoppie");
+                settingHoppieLogon = SettingsFile.Read("LogonCode", "Hoppie");
+                settingPrivateEnabled = SettingsFile.Read("Enabled", "RelayPrivate");
+                settingRadioEnabled = SettingsFile.Read("Enabled", "RelayRadio");
+
+                // Validate values
+                if (settingPushoverToken == null || settingPushoverUser == null)
+                {
+                    SendDebug("Pushover API key or user key not set. Check your vPilot-Pushover.ini");
+                }
+
+                if (settingHoppieEnabled == "1" && settingHoppieLogon == null)
+                {
+                    SendDebug("Hoppie logon code not set. Check your vPilot-Pushover.ini");
+                }
+
+                // Debug print
+                SendDebug($"PushoverToken: {SettingsFile.Read("ApiKey", "Pushover")}");
+                SendDebug($"PushoverUser: {SettingsFile.Read("UserKey", "Pushover")}");
+                SendDebug($"HoppieEnabled: {SettingsFile.Read("Enabled", "Hoppie")}");
+                SendDebug($"HoppieLogon: {SettingsFile.Read("LogonCode", "Hoppie")}");
+                SendDebug($"PrivateEnabled: {SettingsFile.Read("Enabled", "RelayPrivate")}");
+                SendDebug($"RadioEnabled: {SettingsFile.Read("Enabled", "RelayRadio")}");
+
+                SettingsLoaded = true;
+
+            }
+            else
+            {
+                SendDebug("Registry key not found. Is vPilot installed correctly?");
+            }
+
+        }
+
+    }
+}
